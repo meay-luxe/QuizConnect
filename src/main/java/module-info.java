e7f14896a5440087a91440c69ce9768339e9d83a
meay-luxe <185331879+meay-luxe@users.noreply.github.com>
2026-05-10 02:57:55 +0300

    changes in file structure

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,8 @@ module com.example.quizconnect {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.quizconnect to javafx.fxml;
-    opens com.example.quizconnect.controllers to javafx.fxml;  
+    opens com.example.quizconnect.controllers to javafx.fxml;
 
     exports com.example.quizconnect;
 }
